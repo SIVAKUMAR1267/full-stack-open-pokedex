977aa177bb077ea8978b937c4ca539bd54e24638
SIVAKUMAR1267 <sivakumar.r.6324@gmail.com>
2025-11-12 11:42:44 +0530

    ex 11.12 (1)

--- app.js
+++ app.js
@@ -10,5 +10,7 @@ app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
